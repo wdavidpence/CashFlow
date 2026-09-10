cf38d885702feb73f9f0b38edc76c4da54b78a2b
David Pence <davidpence@Davids-MacBook-Pro.local>
2026-09-10 15:52:06 -0400

    Build protected deal calculator vertical slice

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='cashflow-shell-v1';const SHELL=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok&&new URL(e.request.url).origin===self.location.origin)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request).then(r=>r||new Response('CashFlow is offline',{status:503}))))});
